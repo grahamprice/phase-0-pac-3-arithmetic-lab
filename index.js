600b662b43588a1024b1517948160b31bfa07ad0
Graham <thegrahamprice@icloud.com>
2022-06-10 14:16:56 -0600

    commit tree 3645ec11f6a32f3a7c204e5d79625b680335cb28

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a,b)  {
+    return a + b;
+};
+function subtract(a,b){
+return a - b 
+};
+function multiply(a,b){
+return a * b
+};
+function divide(a,b){
+return a / b
+};
+
+let n = 5
+function increment(n){
+    n+= 4
+    return n;
+        
+}
+
